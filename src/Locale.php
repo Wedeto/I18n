1e9afa2ad17a55e05f995fe4fa5f12a60f9a3e05
Egbert van der Wal <ewal@pointpro.nl>
2017-05-12 13:54:47 +0200

    Locale class tested

--- src/Locale.php
+++ src/Locale.php
@@ -31,79 +31,223 @@ namespace Wedeto\I18n;
 
 use Wedeto\Util\Hook;
 
+/**
+ * Locale represents a Locale identifier. It will be validated to have a correct syntax,
+ * and will provide names and information about the locale.
+ *
+ * For more information about the underlying Locale system, see:
+ * http://userguide.icu-project.org/locale
+ */
 class Locale
 {
     protected $locale;
-    protected $data = [];
 
+    /** Internal cache for fallback locale list */
+    private $fallback_list = null;
+
+    /** Internal cache for display names */
+    private $data = [];
+
+    /**
+     * Construct the locale object. The locale will be specified
+     * by a valid Locale string. The locale will be canonicalized, using
+     * level 2 locatization as described in: 
+     * 
+     * http://userguide.icu-project.org/locale#TOC-Canonicalization
+     *
+     * @throws I18nException If the canonicalization fails or an empty locale
+     * is specified.
+     */
     public function __construct(string $locale)
     {
         if (empty($locale))
             throw new I18nException("Empty locale: $locale");
+
+        if (preg_match("/[^a-zA-Z_@=-]/", $locale) || strlen($locale) > 32)
+            throw new I18nException("Invalid locale: $locale");
 
         $this->locale = \Locale::canonicalize($locale);
         if (empty($this->locale))
             throw new I18nException("Invalid locale: $locale");
     }
 
+    /**
+     * Return a Locale object for the default locale, as set by
+     * \Locale::setDefault, or in PHP's INI Value default_locale.
+     */
+    public static function getDefault()
+    {
+        return new Locale(\Locale::getDefault());
+    }
+
+    /**
+     * @return string The full locale set
+     */
     public function getLocale()
     {
         return $this->locale;
     }
 
+    /**
+     * @return array A list of fallback locales, mainly useful for translations.
+     *               The topmost will be the most specific one, dropping one level
+     *               of specifity for each new item, until only the language remains.
+     */
+    public function getFallbackList()
+    {
+        if ($this->fallback_list === null)
+        {
+            $language = $this->getLanguage(); 
+            $region = $this->getRegion();
+            $script = $this->getScript();
+
+            $data = ["language" => $this->getLanguage()];
+            if (!empty($script))
+                $data['script'] = $script;
+            if (!empty($region))
+                $data['region'] = $region;
+
+            $variants = $this->getVariants();
+            foreach ($variants as $idx => $var)
+                $data['variant' . ($idx + 1)] = $var;
+
+            $this->fallback_list = [];
+            while (count($data) > 0)
+            {
+                $locale = \Locale::composeLocale($data);
+                $this->fallback_list[] = new Locale($locale);
+                array_pop($data); 
+            }
+        }
+
+        return $this->fallback_list;
+    }
+
+    /**
+     * @return string The language of the locale. 
+     */
     public function getLanguage()
     {
-        return $this->getDatum('language', 'GetLanguage', 'getPrimaryLanguage');
+        return \Locale::getPrimaryLanguage($this->locale);
     }
 
+    /**
+     * @return string The region of the locale. May be empty if not set in the locale.
+     */
     public function getRegion()
     {
-        return $this->getDatum('region', 'GetRegion', 'getRegion');
+        return \Locale::getRegion($this->locale);
     }
 
-    public function getVariant()
+    /**
+     * @return string The variants of the locale. May be an empty array if none are set.
+     */
+    public function getVariants()
     {
-        return $this->getDatum('variant', 'GetVariant', 'getVariant');
+        $vars = \Locale::getAllVariants($this->locale);
+        return empty($vars) ? [] : $vars;
     }
 
+    /**
+     * @return string The script of the locale. May be empty if not set in the locale.
+     */
     public function getScript()
     {
-        return $this->getDatum('script', 'GetScript', 'getScript');
+        return \Locale::getScript($this->locale);
     }
 
-    public function getDisplayLanguage()
+    /**
+     * @return array A list of keywords in the locale specifier, such as collations, currencies etc.
+     */
+    public function getKeywords()
     {
-        return $this->getDatum('display_language', 'GetDisplayLanguage', 'getDisplayLanguage');
+        $kws = \Locale::getKeywords($this->locale);
+        if (empty($kws))
+            return [];
+        return $kws;
     }
 
-    public function getDisplayName()
+    /**
+     * Obtain a user friendly name for the locale specification
+     *
+     * @param Locale $locale The locale to display in. May be null to use the default
+     * @return string The locale display name.
+     */
+    public function getDisplayName(Locale $in_locale = null)
     {
-        return $this->getDatum('display_name', 'GetDisplayName', 'getDisplayName');
+        return $this->getDisplayItem('display_name', 'GetDisplayName', 'getDisplayName', $in_locale);
     }
 
-    public function getDisplayRegion()
+    /**
+     * Obtain a user friendly name for the locale language
+     *
+     * @param Locale $locale The langauge to display in. May be null to use the default
+     * @return string The language display name.
+     */
+    public function getDisplayLanguage(Locale $in_locale = null)
     {
-        return $this->getDatum('display_region', 'GetDisplayRegion', 'getDisplayRegion');
+        return $this->getDisplayItem('display_language', 'GetDisplayLanguage', 'getDisplayLanguage', $in_locale);
     }
 
-    public function getDisplayScript()
+    /**
+     * Obtain a user friendly name for the region.
+     *
+     * @param Locale $locale The locale to display in. May be null to use the default
+     * @return string The display region name. Can be an empty string when no region is set in the locale
+     */
+    public function getDisplayRegion(Locale $in_locale = null)
     {
-        return $this->getDatum('display_script', 'GetDisplayScript', 'getDisplayScript');
+        return $this->getDisplayItem('display_region', 'GetDisplayRegion', 'getDisplayRegion', $in_locale);
     }
 
-    public function getDisplayVariant()
+    /**
+     * Obtain a user friendly name for the script.
+     *
+     * @param Locale $locale The locale to display in. May be null to use the default
+     * @return string The display script name. Can be an empty string when no script is set in the locale
+     */
+    public function getDisplayScript(Locale $in_locale = null)
     {
-        return $this->getDatum('display_variant', 'GetDisplayVariant', 'getDisplayVariant');
+        return $this->getDisplayItem('display_script', 'GetDisplayScript', 'getDisplayScript', $in_locale);
     }
 
-    protected function getDatum(string $name, string $hook, string $method)
+    /**
+     * Obtain a user friendly name for the variant.
+     *
+     * @param Locale $locale The locale to display in. May be null to use the default
+     * @return string The display variant name. Can be an empty string when no variant is set in the locale
+     */
+    public function getDisplayVariant(Locale $in_locale = null)
     {
-        if (!isset($this->data[$name]))
+        return $this->getDisplayItem('display_variant', 'GetDisplayVariant', 'getDisplayVariant', $in_locale);
+    }
+
+    /**
+     * Internal helper method that caches a datum and requests it when it is not available.
+     * When requesting, the default is the Locale function, but this may be altered by
+     * adding a hook. This can be used to provide display names for
+     * non-standard languages, for example.
+     *
+     * @param string $name The datum to get
+     * @param string $hook The name of the Hook to execute
+     * @param string $method The method in the Locale class to call to obtain the answer
+     * @return mixed The resulting information
+     */
+    protected function getDisplayItem(string $name, string $hook, string $method, Locale $in_locale = null)
+    {
+        $in_locale = $in_locale ?: Locale::getDefault();
+        $in_locale = $in_locale->getLocale();
+        if (!isset($this->data[$in_locale][$name]))
         {
-            $datum = !empty($method) ? Locale::$method($this->locale) : '';
-            $response = Hook::execute("Wedeto.I18n.Locale." . $hook, ['locale' => $this->locale, $name => $datum]);
-            $this->data[$name] = $resp[$name];
+            $datum = !empty($method) ? \Locale::$method($this->locale, $in_locale) : '';
+            $response = Hook::execute("Wedeto.I18n.Locale." . $hook, ['locale' => $this->locale, $name => $datum, 'in_locale' => $in_locale]);
+            $this->data[$in_locale][$name] = $response[$name];
         }
-        return $this->data[$name];
+        return $this->data[$in_locale][$name];
+    }
+
+    public function __toString()
+    {
+        return $this->locale;
     }
 }
---
+++ tests/LocaleTest.php
@@ -0,0 +1,227 @@
+<?php
+/*
+This is part of Wedeto, the WEb DEvelopment TOolkit.
+It is published under the BSD 3-Clause License.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer. Redistributions in binary form
+must reproduce the above copyright notice, this list of conditions and the
+following disclaimer in the documentation and/or other materials provided with
+the distribution. Neither the name of Zend or Rogue Wave Software, nor the
+names of its contributors may be used to endorse or promote products derived
+from this software without specific prior written permission. THIS SOFTWARE IS
+PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+*/
+
+namespace Wedeto\I18n;
+
+use PHPUnit\Framework\TestCase;
+
+class LocaleTest extends TestCase
+{
+    public function setUp()
+    {
+        \Locale::setDefault('en_US');
+    }
+
+    public function testLocaleOnlyLanguage()
+    {
+        $l = new Locale('en');
+        $dl = new Locale('nl');
+        
+        $this->assertEquals('en', $l->getLocale());
+        $this->assertEquals('en', $l->getLanguage());
+        $this->assertEmpty($l->getRegion());
+        $this->assertEmpty($l->getScript());
+        $this->assertEmpty($l->getVariants());
+        $this->assertEmpty($l->getKeywords());
+        $this->assertEquals($l->getLocale(), (string)$l);
+
+        $this->assertEquals('English', $l->getDisplayName());
+        $this->assertEquals('Engels', $l->getDisplayName($dl));
+        $this->assertEquals('English', $l->getDisplayLanguage());
+        $this->assertEquals('Engels', $l->getDisplayLanguage($dl));
+
+        $this->assertEmpty($l->getDisplayRegion());
+        $this->assertEmpty($l->getDisplayScript());
+        $this->assertEmpty($l->getDisplayVariant());
+
+        $this->assertEqualLocaleList(['en'], $l->getFallbackList());
+    }
+
+    public function testLocaleWithRegion()
+    {
+        $l = new Locale('en_US');
+        $dl = new Locale('nl');
+        
+        $this->assertEquals('en_US', $l->getLocale());
+        $this->assertEquals('en', $l->getLanguage());
+        $this->assertEquals('US', $l->getRegion());
+        $this->assertEmpty($l->getScript());
+        $this->assertEmpty($l->getVariants());
+        $this->assertEmpty($l->getKeywords());
+        $this->assertEquals($l->getLocale(), (string)$l);
+
+        $this->assertEquals('English (United States)', $l->getDisplayName());
+        $this->assertEquals('Engels (Verenigde Staten)', $l->getDisplayName($dl));
+        $this->assertEquals('English', $l->getDisplayLanguage());
+        $this->assertEquals('Engels', $l->getDisplayLanguage($dl));
+
+        $this->assertEquals('United States', $l->getDisplayRegion());
+        $this->assertEmpty($l->getDisplayScript());
+        $this->assertEmpty($l->getDisplayVariant());
+
+        $this->assertEqualLocaleList(['en_US', 'en'], $l->getFallbackList());
+    }
+
+    public function testLocaleWithRegionAndVariant()
+    {
+        $l = new Locale('en_US_Military');
+        $dl = new Locale('nl');
+        
+        $this->assertEquals('en_US_MILITARY', $l->getLocale());
+        $this->assertEquals('en', $l->getLanguage());
+        $this->assertEquals('US', $l->getRegion());
+        $this->assertEmpty($l->getScript());
+        $this->assertEquals(['MILITARY'], $l->getVariants());
+        $this->assertEmpty($l->getKeywords());
+        $this->assertEquals($l->getLocale(), (string)$l);
+
+        $this->assertEquals('English (United States, MILITARY)', $l->getDisplayName());
+        $this->assertEquals('Engels (Verenigde Staten, MILITARY)', $l->getDisplayName($dl));
+        $this->assertEquals('English', $l->getDisplayLanguage());
+        $this->assertEquals('Engels', $l->getDisplayLanguage($dl));
+        $this->assertEquals('United States', $l->getDisplayRegion());
+        $this->assertEquals('MILITARY', $l->getDisplayVariant());
+
+        $this->assertEmpty($l->getDisplayScript());
+
+        $this->assertEqualLocaleList(['en_US_MILITARY', 'en_US', 'en'], $l->getFallbackList());
+    }
+
+    public function testLocaleWithLanguageAndScript()
+    {
+        $l = new Locale('zh_CHT');
+        $dl = new Locale('nl');
+        
+        $this->assertEquals('zh_Hant', $l->getLocale());
+        $this->assertEquals('zh', $l->getLanguage());
+        $this->assertEmpty($l->getRegion());
+        $this->assertEquals('Hant', $l->getScript());
+        $this->assertEmpty($l->getVariants());
+        $this->assertEmpty($l->getKeywords());
+        $this->assertEquals($l->getLocale(), (string)$l);
+
+        $this->assertEquals('Chinese (Traditional)', $l->getDisplayName());
+        $this->assertEquals('Chinees (traditioneel)', $l->getDisplayName($dl));
+        $this->assertEquals('Chinese', $l->getDisplayLanguage());
+        $this->assertEquals('Chinees', $l->getDisplayLanguage($dl));
+        $this->assertEmpty($l->getDisplayRegion());
+        $this->assertEmpty($l->getDisplayVariant());
+        $this->assertEquals('Traditional Han', $l->getDisplayScript());
+
+        $this->assertEqualLocaleList(['zh_Hant', 'zh'], $l->getFallbackList());
+    }
+
+    public function testLocaleWithLanguageAndScriptAndRegion()
+    {
+        $l = new Locale('zh_Hant_CN');
+        $dl = new Locale('nl');
+        
+        $this->assertEquals('zh_Hant_CN', $l->getLocale());
+        $this->assertEquals('zh', $l->getLanguage());
+        $this->assertEquals('CN', $l->getRegion());
+        $this->assertEquals('Hant', $l->getScript());
+        $this->assertEmpty($l->getVariants());
+        $this->assertEmpty($l->getKeywords());
+        $this->assertEquals($l->getLocale(), (string)$l);
+
+        $this->assertEquals('Chinese (Traditional, China)', $l->getDisplayName());
+        $this->assertEquals('Chinees (traditioneel, China)', $l->getDisplayName($dl));
+        $this->assertEquals('Chinese', $l->getDisplayLanguage());
+        $this->assertEquals('Chinees', $l->getDisplayLanguage($dl));
+        $this->assertEquals('China', $l->getDisplayRegion());
+        $this->assertEmpty($l->getDisplayVariant());
+        $this->assertEquals('Traditional Han', $l->getDisplayScript());
+
+        $this->assertEqualLocaleList(['zh_Hant_CN', 'zh_Hant', 'zh'], $l->getFallbackList());
+    }
+
+    public function testLocaleWithLanguageAndRegionAndCurrency()
+    {
+        $l = new Locale('en_US@currency=USD');
+        $dl = new Locale('nl');
+        
+        $this->assertEquals('en_US@currency=USD', $l->getLocale());
+        $this->assertEquals('en', $l->getLanguage());
+        $this->assertEquals('US', $l->getRegion());
+        $this->assertEmpty($l->getScript());
+        $this->assertEmpty($l->getVariants());
+        $this->assertEquals(['currency' => 'USD'], $l->getKeywords());
+        $this->assertEquals($l->getLocale(), (string)$l);
+
+        $this->assertEquals('English (United States, Currency=US Dollar)', $l->getDisplayName());
+        $this->assertEquals('Engels (Verenigde Staten, valuta=Amerikaanse dollar)', $l->getDisplayName($dl));
+        $this->assertEquals('English', $l->getDisplayLanguage());
+        $this->assertEquals('Engels', $l->getDisplayLanguage($dl));
+        $this->assertEquals('United States', $l->getDisplayRegion());
+        $this->assertEmpty($l->getDisplayVariant());
+        $this->assertEmpty($l->getDisplayScript());
+
+        $this->assertEqualLocaleList(['en_US', 'en'], $l->getFallbackList());
+    }
+
+    public function testInvalidLocale()
+    {
+        $invalid_locales = [
+            '' => 'Empty locale',
+            'Engels (Verenigde Staten)' => 'Invalid locale',
+            'foo # bar Boo Baz , ' => 'Invalid locale',
+            'foo - bar' => 'Invalid locale',
+            '!@#!@#' => 'Invalid locale',
+            'this locale is a string that is too long to be a locale Therefore it should throw an error' => 'Invalid locale'
+        ];
+
+        foreach ($invalid_locales as $locale => $error)
+        {
+            $thrown = false;
+            $lstr = $locale;
+            try
+            {
+                $l = new Locale($locale);
+                $lstr .= " -> " . $l->getLocale();
+            }
+            catch (I18nException $e)
+            {
+                $this->assertContains($error, $e->getMessage());
+                $thrown = true;
+            }
+            $this->assertTrue($thrown, "Check that exception was thrown when parsing " . $lstr);
+        }
+    }
+
+    public function assertEqualLocaleList(array $expected, array $list)
+    {
+        $this->assertEquals(count($expected), count($list));
+
+        for ($i = 0; $i < count($list); ++$i)
+        {
+            $obj = $list[$i];
+            $locale = $expected[$i];
+            $this->assertEquals($list[$i]->getLocale(), $expected[$i]);
+        }
+    }
+}
+
